aa4c9967652eca7c4d0f43b46ad989c608ffe85e
C.Rondeau <rondeau.clement@gmail.com>
2018-11-12 23:26:02 +0100

    Add enemy spawning system

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+	void Start () {
+        StartCoroutine(SpawnEnemy());
+	}
+	
+    IEnumerator SpawnEnemy()
+    {
+        Vector2 position = RandomPointOnUnitCircle(30f);
+        // TODO : Check if there is already an enemy at this position
+        Instantiate(enemyPrefab, new Vector3(position.x, 1f, position.y), Quaternion.identity);
+
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private Vector2 RandomPointOnUnitCircle(float radius)
+    {
+        float angle = Random.Range(0f, Mathf.PI * 2);
+        float x = Mathf.Sin(angle) * radius;
+        float y = Mathf.Cos(angle) * radius;
+
+        return new Vector2(x, y);
+    }
+}
